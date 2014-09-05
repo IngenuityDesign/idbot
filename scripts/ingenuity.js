d0b232a0a9fdc4d0e2cc1d342fcd139c8757e0ca
Stephen Parente <webmakersteve@gmail.com>
2014-09-05 19:57:48 -0400

    Added phantomjs module

--- scripts/ingenuity.js
+++ scripts/ingenuity.js
@@ -31,5 +31,22 @@ module.exports = function(robot) {
 
     robot.hear(/is ebayinc done( yet)?/i, function(msg) {
       msg.reply("Nope. Never");
-    })
+    });
+
+    robot.hear(/screenshot (.*)/i, function(msg) {
+      var path = require('path')
+      var childProcess = require('child_process')
+      var phantomjs = require('phantomjs')
+      var binPath = phantomjs.path
+
+      var childArgs = [
+        path.join(__dirname, 'phantomjs-script.js'),
+        'some other argument (passed to phantomjs script)'
+      ]
+
+      childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+        // handle results
+        msg.reply(stdout);
+      })
+    });
 }
