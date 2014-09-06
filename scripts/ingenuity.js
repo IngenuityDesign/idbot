9fe7c0f4768fe020a20d5acb8b626ed73d5766ef
Stephen Parente <webmakersteve@gmail.com>
2014-09-05 20:31:09 -0400

    Added screenshot functionality

--- scripts/ingenuity.js
+++ scripts/ingenuity.js
@@ -17,6 +17,22 @@ var apps = {
   }
 }
 
+var s3 = require('s3');
+
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_ACCESS_SECRET,
+    // any other options are passed to new AWS.S3()
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+  },
+});
+
 module.exports = function(robot) {
     robot.respond(/deploy ([^ ]+)/i, function(msg){
         var application = msg.match[1];
@@ -40,13 +56,42 @@ module.exports = function(robot) {
       var binPath = phantomjs.path
 
       var childArgs = [
-        path.join(__dirname, 'phantomjs-script.js'),
-        'some other argument (passed to phantomjs script)'
+        path.join(path.dirname(__dirname), 'sh/phantomjs-screenshot.js'),
+        'http://google.com/',
+        'sh/test.png'
       ]
 
       childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
         // handle results
-        msg.reply(stdout);
+        if (stderr) {
+
+        }
+        var filename = Math.floor(Math.random() * 1000000 ) + ".png";
+
+        var params = {
+          localFile: "sh/test.png",
+
+          s3Params: {
+            ACL: 'public-read',
+            Bucket: "idbot",
+            Key: filename,
+            // other options supported by putObject, except Body and ContentLength.
+            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+          },
+        };
+        var uploader = client.uploadFile(params);
+        uploader.on('error', function(err) {
+          msg.reply("Couldn't upload the screenshot.")
+        });
+
+        uploader.on('progress', function() {
+          //console.log("progress", uploader.progressMd5Amount,
+          //          uploader.progressAmount, uploader.progressTotal);
+        });
+
+        uploader.on('end', function() {
+          msg.reply(s3.getPublicUrlHttp(params.s3Params.Bucket, params.s3Params.Key));
+        });
       })
     });
 }
